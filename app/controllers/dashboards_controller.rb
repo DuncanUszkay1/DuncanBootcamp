d8736e43ba183fc9247517aecd0a2eff1a602301
Duncan Uszkay <duncan.uszkay@shopify.com>
2018-01-12 10:54:22 -0500

    Added CRUD capabilities for the users model

---
+++ app/controllers/dashboards_controller.rb
@@ -0,0 +1,16 @@
+class DashboardsController < ApplicationController
+  def hello
+  end
+
+  def hi
+  end
+
+  def personalized
+    @name = params[:name]
+  end
+
+  def reversed
+    @name = params[:name].reverse
+    render :personalized
+  end
+end
